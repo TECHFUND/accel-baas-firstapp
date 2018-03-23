df9d5a8b0281b890244b96c1258ef7cc62a5c279
Yuta <milkywebboy@gmail.com>
2018-03-24 08:41:35 +0900

    minimum debug.

--- contract.js
+++ contract.js
@@ -4,9 +4,9 @@ var contract1_wallet = "AB7ks7n7oe";	// wallet
 var contract1_balance = blockchainAccess.getWallet(contract1_wallet);	// balance
 
 // When contract wallet balance reaches 3 or more, send 2 to administrator
-if (3 <= (contract1_balance.balance * Math.pow(10, 8))) {
+if ((3 * Math.pow(10, 8)) <= contract1_balance.balance) {
 	ret = true;
-	blockchainAccess.createTransaction(admin_wallet, 2, admin_mnemonic, "contract1", function(){
+	blockchainAccess.createTransaction(admin_wallet, 2, contract1_wallet, "contract1", function(){
 		// done
 	});
 }
