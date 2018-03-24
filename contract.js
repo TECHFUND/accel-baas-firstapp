89d3950946458abb7452bbac3d86025887aaccae
Yuta <milkywebboy@gmail.com>
2018-03-24 11:03:24 +0900

    minimum debug.

--- contract.js
+++ contract.js
@@ -1,12 +1,12 @@
 var admin_wallet = "ABb5uxz3tm";
 
 var contract1_wallet = "AB7ks7n7oe";	// wallet
-var contract1_balance = blockchainAccess.getWallet(contract1_wallet);	// balance
-
-// When contract wallet balance reaches 3 or more, send 2 to administrator
-if ((3 * Math.pow(10, 8)) <= contract1_balance.balance) {
-	ret = true;
-	blockchainAccess.createTransaction(admin_wallet, 2, contract1_wallet, "contract1", function(){
-		// done
-	});
-}
+blockchainAccess.getWallet(contract1_wallet, function(contract1_balance) {
+	// When contract wallet balance reaches 3 or more, send 2 to administrator
+	if ((3 * Math.pow(10, 8)) <= contract1_balance.balance) {
+		ret = true;
+		blockchainAccess.createTransaction(admin_wallet, 2, contract1_wallet, "contract1", function(){
+			// done
+		});
+	}
+});
